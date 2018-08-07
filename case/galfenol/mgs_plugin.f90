444962c18b964ffc612e955099cdc9eac4630a37
Juhani Kataja <juhani.kataja@csc.fi>
2018-08-07 16:21:29 +0300

    fix in mgs_plugin.f90

--- case/galfenol/mgs_plugin.f90
+++ case/galfenol/mgs_plugin.f90
@@ -16,7 +16,7 @@
 
 subroutine mgs_H(B1, B2, B3, e11, e12, e13, e21, e22, e23, e31, e32, e33, h_out)
   use types
-  use master
+  use MgsContainer
   implicit none
   REAL(KIND=dp), intent(in) :: B1, B2, B3, e11, e12, e13, e21, e22, e23, e31, e32, e33
   REAL(KIND=dp), intent(out), dimension(1:3, 1:1) :: h_out
@@ -25,7 +25,7 @@ end subroutine
 
 subroutine mgs_dHdB(B1, B2, B3, e11, e12, e13, e21, e22, e23, e31, e32, e33, dhdb_out)
   use types
-  use master
+  use MgsContainer
   implicit none
   REAL(KIND=dp), intent(in) :: B1, B2, B3, e11, e12, e13, e21, e22, e23, e31, e32, e33
   REAL(KIND=dp), intent(out), dimension(1:3, 1:3) :: dhdb_out
@@ -34,7 +34,7 @@ end subroutine
 
 subroutine mgs_S(B1, B2, B3, e11, e12, e13, e21, e22, e23, e31, e32, e33, sigma_out)
   use types
-  use master
+  use MgsContainer
   implicit none
   REAL(KIND=dp), intent(in) :: B1, B2, B3, e11, e12, e13, e21, e22, e23, e31, e32, e33
   REAL(KIND=dp), intent(out), dimension(1:3, 1:3) :: sigma_out
@@ -43,7 +43,7 @@ end subroutine
 
 subroutine mgs_dSde(B1, B2, B3, e11, e12, e13, e21, e22, e23, e31, e32, e33, dsde_out)
   use types
-  use master
+  use MgsContainer
   implicit none
   REAL(KIND=dp), intent(in) :: B1, B2, B3, e11, e12, e13, e21, e22, e23, e31, e32, e33
   REAL(KIND=dp), intent(out), dimension(1:9, 1:9) :: dsde_out
