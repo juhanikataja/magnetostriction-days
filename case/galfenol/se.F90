44d7546f3d5811a8d600c0b542a4df94a798c12d
Juha Jeronen <juha.jeronen@tut.fi>
2018-09-05 15:44:02 +0300

    stress-strain relation, also E

--- case/galfenol/se.F90
+++ case/galfenol/se.F90
@@ -2,7 +2,7 @@ program se
 use types
 use mgscontainer
 implicit none
-real(kind=dp) :: e(1000), S_se(3,3)
+real(kind=dp) :: e(1000), S_se(3,3), dSde_se(9,9)
 
 integer :: n
 
@@ -12,5 +12,8 @@ do n = 1,1000
   call S_public(0d0, 0d0, 0d0, e(n), 0d0, 0d0, 0d0, 0d0, 0d0, S_se)
   print *, e(n), S_se(1,1)
 end do
+
+call dS_deps_public(0d0, 0d0, 0d0, dSde_se)
+print *, dSde_se(1,1)
 
 end program
